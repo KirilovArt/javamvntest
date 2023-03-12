9e97f77b2f027e673939c27f4e406ce938c17499
Artem <art_kir_95@mail.ru>
2023-03-12 05:23:58 +0300

    add

---
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -0,0 +1,12 @@
+package ru.netology.javaqa;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = true;
+        long bonus = service.calculate(amount, registered);
+        System.out.println(bonus);
+    }
+
+}
---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,83 @@
+import ru.netology.javaqa.BonusService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusServiceTest {
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndNoAmount() {
+        BonusService service = new BonusService();
+
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndNoAmount() {
+        BonusService service = new BonusService();
+
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+}
